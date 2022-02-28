4fdfe07cde52456539dbc7dfefe8b22232a1b704
ikrzmcxx <99166161+ikrzmcxx@users.noreply.github.com>
2022-02-28 16:38:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head> <title> Perfomance task presented by group 5 </title> </head>
+<img src="programming.gif" alt="programming man"
+style="float:right;width:100px;height:100px;">
+<body style="background-color:green;">
+
+<h1 style="font-size:300%;"> Performance task </h1>
+
+<h2> Links for the other subjects: </h2>
+
+<h2> Communication Arts in English 9 (CAE) </h2>
+<p> Link: </p>
+<a href="https://ptcaeqq.herokuapp.com/"> CAELINK </a>
+<hr>
+<h2> Christian Living Education 9 ( CLE ) </h2>
+
+<table>
+   <tr>
+     <td> Link: </td>
+     <p> <a href="https://cleas.herokuapp.com/"> CLELINK </a> </p>
+   </tr>
+</table>
+
+<hr>
+<h2> Mathematics 9 ( Math ) </h2>
+<table>
+  <tr>     
+    <td> Link: </td>
+    <p> <a href="https://heyuaaa.herokuapp.com/"> MATHLINK </a> </p>
+  </tr>
+</table>
+
+</body>
+</html>
